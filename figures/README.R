01749901093f7e93b716bb0a9f3274efc8ec9ed7
davidbomze <davidbomze@gmail.com>
2020-07-02 18:52:07 +0300

    Create README.R

---
+++ figures/README.R
@@ -0,0 +1,10 @@
+# This is the code to calculate the SIFI using the
+# 'myeloid' and 'retinopathy' sample datasets from the 'survival' package
+
+source("../SIFI.R")
+sifi(myeloid[, c("futime","death","trt")], plot_iteration = T, file_iteration = "myeloid_sifi.pdf")    # Default SIFI strategy
+sifi_all(myeloid[ , c("futime","death","trt")], plot_iteration = T, file_prefix = "myeloid_sifi")      # All SIFI strategies
+sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "argon", plot_iteration = T, file_iteration = "retinopathy_sifi_argon.pdf")    # Default strategy, define experimental arm
+sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "xenon", plot_iteration = T, file_iteration = "retinopathy_sifi_xenon.pdf")    # Default strategy, define experimental arm
+sifi(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_iteration = "retinopathy_sifi_agnostic.pdf")    # Default strategy, without defining experimental arm ('agnostic')
+sifi_all(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_prefix = "retinopathy_sifi")    # All SIFI strategies
