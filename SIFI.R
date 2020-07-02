3259c95aac341294699222b6a16094d4304b14dd
davidbomze <davidbomze@gmail.com>
2020-07-02 20:31:42 +0300

    Update SIFI.R

--- SIFI.R
+++ SIFI.R
@@ -1,12 +1,7 @@
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# This is the code to calculate the SIFI using the
-# 'myeloid' and 'retinopathy' sample datasets from the 'survival' package
-# sifi(myeloid[, c("futime","death","trt")], plot_iteration = T, file_iteration = "myeloid_sifi_default.pdf")    # Default SIFI strategy
-# sifi_all(myeloid[ , c("futime","death","trt")], plot_iteration = T, file_prefix = "myeloid_sifi")      # All SIFI strategies
-# sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "argon", plot_iteration = T, file_iteration = "retinopathy_sifi_default_argon.pdf")    # Default SIFI strategy, define experimental arm
-# sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "xenon", plot_iteration = T, file_iteration = "retinopathy_sifi_default_xenon.pdf")    # Default SIFI strategy, define experimental arm
-# sifi(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_iteration = "retinopathy_sifi_default_agnostic.pdf")    # Default SIFI strategy, without defining experimental arm ('agnostic' approach will choose 'xenon' as experimental arm in this case)
-# sifi_all(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_prefix = "retinopathy_sifi")    # All SIFI strategies
+# This is the coded used to calculate SIFI
+# For specific examples, see 'figures' folder
+# Author: David Bomze, 2020
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -371,11 +366,12 @@ sifi_all <- function(sv_data, treatment_arm = NULL,  # 'sv_data' should be (1) t
   stat_test <- match.arg(stat_test)
   
   # Just need a data input, and run it 4 times
-  #@@@@ WE NOW HAVE 4 OPTIONS (2x2)
+  #@@@@@@@@@@ WE NOW HAVE 4 OPTIONS (2x2)
   # 1) Re-designate the best responder  (longest time)  from experimental to control group
   # 2) Re-designate the worst responder (shortest time) from control to experimental group
   # 3) Flip that responder, 4) Clone that responder
-    
+  #@@@@@@@@@@
+  
   # Create list to store SIFI
   mega_sifi <- vector("list", 4)
   names(mega_sifi) <- expand.grid(operation = c("flip","clone"), direction = c("best","worst")) %>%
