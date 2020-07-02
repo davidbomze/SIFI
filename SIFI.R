7bba08a2163c1dc3ff7c198dc17fff970a4ace54
davidbomze <davidbomze@gmail.com>
2020-07-02 18:58:16 +0300

    Update SIFI.R

--- SIFI.R
+++ SIFI.R
@@ -1,11 +1,11 @@
 ##################################################
 # This is the code to calculate the SIFI using the
 # 'myeloid' and 'retinopathy' sample datasets from the 'survival' package
-# sifi(myeloid[, c("futime","death","trt")], plot_iteration = T, file_iteration = "myeloid_sifi.pdf")    # Default SIFI strategy
+# sifi(myeloid[, c("futime","death","trt")], plot_iteration = T, file_iteration = "myeloid_sifi_default.pdf")    # Default SIFI strategy
 # sifi_all(myeloid[ , c("futime","death","trt")], plot_iteration = T, file_prefix = "myeloid_sifi")      # All SIFI strategies
-# sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "argon", plot_iteration = T, file_iteration = "retinopathy_sifi_argon.pdf")    # Default strategy, define experimental arm
-# sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "xenon", plot_iteration = T, file_iteration = "retinopathy_sifi_xenon.pdf")    # Default strategy, define experimental arm
-# sifi(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_iteration = "retinopathy_sifi_agnostic.pdf")    # Default strategy, without defining experimental arm ('agnostic')
+# sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "argon", plot_iteration = T, file_iteration = "retinopathy_sifi_default_argon.pdf")    # Default strategy, define experimental arm
+# sifi(retinopathy[ , c("futime","status","laser")], treatment_arm = "xenon", plot_iteration = T, file_iteration = "retinopathy_sifi_default_xenon.pdf")    # Default strategy, define experimental arm
+# sifi(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_iteration = "retinopathy_sifi_default_agnostic.pdf")    # Default strategy, without defining experimental arm ('agnostic')
 # sifi_all(retinopathy[ , c("futime","status","laser")], plot_iteration = T, file_prefix = "retinopathy_sifi")    # All SIFI strategies
 
 ##################################################
